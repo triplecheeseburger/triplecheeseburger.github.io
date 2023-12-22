691222be7320912f53d37472b193afbce9db2fe7
triplecheeseburger <hangyulkim94@gmail.com>
2023-12-22 21:56:13 +0900

    [feat] make not to shrink

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className='flex flex-row h-7'>
+    <div className='flex flex-row h-7 flex-shrink-0'>
       <HomeButton />
     </div>
   );
